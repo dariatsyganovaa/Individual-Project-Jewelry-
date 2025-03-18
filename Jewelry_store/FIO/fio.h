2b65c6feab39746d83c44739a6ffa3a141e48107
dariatsyganovaa <dariatsyganovaa@yandex.ru>
2025-03-18 18:26:37 +0300

    declaring a class FIO

--- Jewelry_store/FIO/fio.h
+++ Jewelry_store/FIO/fio.h
@@ -1,6 +1,29 @@
 #pragma once
 class FIO {
-	int _first_name;
-	int _second_name;
-	int _last_name;
+	char* _first_name;
+	char* _second_name;
+	char* _last_name;
+
+public:
+	//конструкторы
+	FIO();
+	FIO(const char*, const char*, const char*);
+	FIO(char*, char*, char*);
+	FIO(const FIO&);
+
+	//деструктор
+	~FIO();
+
+	//сеттеры
+	void set_first_name(const char*);
+	void set_second_name(const char*);
+	void set_last_name(const char*);
+
+	//геттеры
+	const char* get_first_name();
+	const char* get_second_name();
+	const char* get_last_name();
+
+private:
+	void check_corrections(const char* name);
 };
